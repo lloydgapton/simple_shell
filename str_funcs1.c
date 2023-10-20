5ef4a8d5660de2cc02050b0f846d947461a29ba0
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-10-20 23:41:25 +0100

    Update str_funcs1.c

--- str_funcs1.c
+++ str_funcs1.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 int _strlen(const char *s);
-char *_strcpy(char *dest, const char *src);
 char *_strcat(char *dest, const char *src);
 char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
 
 /**
  * _strlen - Re
