1784eb8302d85ff082c09bd09a890b31b4f1f3d9
lloydgapton <108632761+lloydgapton@users.noreply.github.com>
2023-10-20 07:28:20 +0100

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,9 +1,8 @@
 #include "shell.h"
-
-alias_t *add_alias_end(alias_t **head, char *name, char *value);
-void free_alias_list(alias_t *head);
 list_t *add_node_end(list_t **head, char *dir);
 void free_list(list_t *head);
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
 
 /**
  * add_alias_end - Adds
